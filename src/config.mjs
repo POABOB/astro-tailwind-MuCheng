edd0bb53fed8e4c7281055813c454f9510b5ae36
POABOB <38785340+POABOB@users.noreply.github.com>
2023-01-12 06:00:25 +0800

    把原本模板中的頁面，刪除blog相關的檔案

--- src/config.mjs
+++ src/config.mjs
@@ -14,7 +14,7 @@ const CONFIG = {
 
   defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
 
-  language: 'en',
+  language: 'zh-tw',
   textDirection: 'ltr',
 
   dateFormatter: new Intl.DateTimeFormat('en', {
@@ -26,35 +26,6 @@ const CONFIG = {
 
   googleAnalyticsId: false, // or "G-XXXXXXXXXX",
   googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
-
-  blog: {
-    disabled: false,
-    postsPerPage: 4,
-
-    list: {
-      pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
-      noindex: false,
-      disabled: false,
-    },
-
-    post: {
-      pathname: '', // empty for /some-post, value for /pathname/some-post
-      noindex: false,
-      disabled: false,
-    },
-
-    category: {
-      pathname: 'category', // set empty to change from /category/some-category to /some-category
-      noindex: true,
-      disabled: false,
-    },
-
-    tag: {
-      pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
-      noindex: true,
-      disabled: false,
-    },
-  },
 };
 
 export const SITE = { ...CONFIG, blog: undefined };
